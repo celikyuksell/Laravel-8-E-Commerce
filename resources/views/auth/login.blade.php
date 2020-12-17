c4642a63dace8b56cce4477f7c6dba313a06654d
celikyuksell <celikyuksell@gmail.com>
2020-12-17 21:04:31 +0300

    Admin Template Implemantation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,6 +16,7 @@
             @csrf
 
             <div>
+
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::get('/home2', function () {
     return view('welcome');
 });
@@ -31,8 +29,8 @@ Route::get('/aboutus', [HomeController::class, 'aboutus'])->name('aboutus');
 //Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+']);
 Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->whereNumber('id')->whereAlpha('name')->name('test');
 
-
-
+// Admin
+Route::get('/admin', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminhome');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
